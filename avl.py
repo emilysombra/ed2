139e1ee463b8f01119914feb6ef3caf40369d46a
Marcos Sombra <markos.vs@live.com>
2019-08-25 19:28:19 -0300

    chupa meu pau avl

--- avl.py
+++ avl.py
@@ -5,27 +5,36 @@ class TreeNode:
         self.right = None
         self.height = 1
 
+    @property
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
 
 class AVLTree:
-    def add(self, root, value):
+    def add_list(self, values, root):
+        for value in values:
+            root = self.add(value, root)
+        return root
+
+    def add(self, value, root=None):
         # Adding element (regular BST insertion)
         if(root is None):
             return TreeNode(value)
-        elif(value == root.value):
+        if(value == root.value):
             return root
         elif(value < root.value):
-            root.left = self.add(root.left, value)
+            root.left = self.add(value, root.left)
         else:
-            root.right = self.add(root.right, value)
+            root.right = self.add(value, root.right)
 
         # update height
         root.height = 1 + max(self.get_height(root.left),
                               self.get_height(root.right))
         balance = self.get_balance(root)
         # doing rotation
-        if(balance > 1 and value < root.value):
+        if(balance > 1 and value < root.left.value):
             return self.rotate_right(root)
-        elif(balance < -1 and value > root.value):
+        elif(balance < -1 and value > root.right.value):
             return self.rotate_left(root)
         elif(balance > 1 and value > root.left.value):
             return self.asym_rotate_left(root)
@@ -118,27 +127,69 @@ class AVLTree:
         temp = str(root.value) + ' '
         temp += self.preorder(root.left) + ' '
         temp += self.preorder(root.right)
+        while('  ' in temp):
+            temp.replace('  ', ' ')
         return temp
+
+    def to_list(self, root):
+        if(root is None):
+            return list(set())
+        base = {root.value}
+        base.update(self.to_list(root.left))
+        base.update(self.to_list(root.right))
+        return list(base)
+
+    def search(self, root, value):
+        if(root is None):
+            return None
+        if(value == root.value):
+            return root
+        elif(value < root.value):
+            return self.search(root.left, value)
+        else:
+            return self.search(root.right, value)
+
+    def delete(self, root, value):
+        if(root is None):
+            return root
+
+        if(value == root.value):
+            lista = self.to_list(root.right)
+            return self.add_list(lista, root.left)
+        elif(value < root.value):
+            root.left = self.delete(root.left, value)
+        else:
+            root.right = self.delete(root.right, value)
+        # update height
+        try:
+            left_height = self.get_height(root.left)
+        except AttributeError:
+            left_height = 0
+        try:
+            right_height = self.get_height(root.right)
+        except AttributeError:
+            right_height = 0
+        root.height = 1 + max(left_height, right_height)
+        # check the balance
+        balance = self.get_balance(root)
+        # doing rotation
+        if(balance > 1):
+            root = self.rotate_right(root)
+            new_balance = self.get_balance(root)
+            if(new_balance < -1):
+                root = self.asym_rotate_right(root)
+        elif(balance < -1):
+            root = self.rotate_left(root)
+            new_balance = self.get_balance(root)
+            if(new_balance > 1):
+                root = self.asym_rotate_left(root)
+
+        return root
 
 
 if __name__ == '__main__':
     tree = AVLTree()
-    root = None
-    root = tree.add(root, 3)
+    root = tree.add_list([10, 5, 15, 7, 3], None)
     print(tree.preorder(root))
-    root = tree.add(root, 8)
-    print(tree.preorder(root))
-    root = tree.add(root, 9)
-    print(tree.preorder(root))
-    root = tree.add(root, 4)
-    print(tree.preorder(root))
-    root = tree.add(root, 16)
-    print(tree.preorder(root))
-    root = tree.add(root, 17)
-    print(tree.preorder(root))
-    root = tree.add(root, 13)
-    print(tree.preorder(root))
-    root = tree.add(root, 7)
-    print(tree.preorder(root))
-    root = tree.add(root, 12)
+    root = tree.delete(root, 5)
     print(tree.preorder(root))
